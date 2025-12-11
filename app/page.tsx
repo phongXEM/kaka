a017484aa58616891b3d3de88beb5d90d1b90b5d
phongXEM <phong@xem.edu.vn>
2025-12-11 17:59:59 +0700

    docs v2

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation v1
+            Documentation v2
           </a>
         </div>
       </main>
