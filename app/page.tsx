f9ab25ca3ccca772c93f18f79aaa8ddbd3c4c5af
phongXEM <phong@xem.edu.vn>
2025-12-16 09:04:47 +0700

    update env

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
+// Client-side và Server-side
+const appName = process.env.NEXT_PUBLIC_APP_NAME;
+
+// Chỉ Server-side (API routes, Server Components)
+const apiSecret = process.env.API_SECRET_KEY;
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -48,7 +53,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy Now
+            Deploy Now {appName}
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
